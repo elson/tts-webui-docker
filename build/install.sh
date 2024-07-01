1904eb99c482ecdba26245c357d5bc1291c904ac
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-07-01 16:29:27 +0200

    Fix protobuf

--- build/install.sh
+++ build/install.sh
@@ -28,7 +28,7 @@ pip3 install -r requirements_styletts2.txt
 pip3 install -r requirements_vall_e.txt
 pip3 install -r requirements_maha_tts.txt
 pip3 install -r requirements_stable_audio.txt
-pip3 install soundfile==0.12.1 protobuf==3.19.4
+pip3 install soundfile==0.12.1 protobuf==5.27.2
 deactivate
 
 # Install the NodeJS dependencies for the TTS Generation WebUI
